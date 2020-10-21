f8c8abc26c11fbb479047839c80b6b6e2f1977af
RichardFuentes369 <javierbaron6@gmail.com>
2020-10-21 11:38:17 -0500

    computador nuevo

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,22 +18,22 @@ use Illuminate\Support\Str;
 */
 
 $factory->define(User::class, function (Faker $faker) {
-	return [
-		'name' => $faker->name,
-		'lastname' => $faker->name,
-		'td' => $faker->randomElement(['CC', 'TI', 'RC', 'CE', 'PA']),
-		'dni' => $faker->randomDigit(),
-		'sexo' => $faker->randomElement(['M', 'F']),
-		'email' => $faker->unique()->safeEmail,
-		'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-		'address' => $faker->address,
-		'isSuperAdmin' => '1',
-		'isAdmin' => '0',
-		'isAtm' => '0',
-		'isWaiter' => '0',
-		'isChef' => '0',
-		'isClient' => '0',
-        'remember_token' => Str::random(10),
-    ];
+  return [
+    'name' => $faker->name,
+    'lastname' => $faker->name,
+    'td' => $faker->randomElement(['CC', 'TI', 'RC', 'CE', 'PA']),
+    'dni' => $faker->randomDigit(),
+    'sexo' => $faker->randomElement(['M', 'F']),
+    'email' => $faker->unique()->safeEmail,
+    'email_verified_at' => now(),
+    'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+    'address' => $faker->address,
+    'isSuperAdmin' => '1',
+    'isAdmin' => '0',
+    'isAtm' => '0',
+    'isWaiter' => '0',
+    'isChef' => '0',
+    'isClient' => '0',
+    'remember_token' => Str::random(10),
+];
 });
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,64 +1,61 @@
 <template>
   <div> 
     <div 
-      id="menu" 
-      class="fixed-top"
+    id="menu" 
+    class="fixed-top"
     >  
-      <span 
-        class="navbar-brand text-white" 
-        href="#"
-      >
-        Restaurante
-      </span>
-      <ul>
-        <!-- <li v-if="usuario_logeado == ''"> -->
-        <li v-if="$route.meta.requiresAuth == false">
-          <router-link 
-            :to="{ name: 'bienvenidos'}" 
-            class="nav-link"
-          >
-            Inicio
-          </router-link>
-        </li>
-        <li v-else>
-          <router-link 
-            :to="{ name: 'role'}" 
-            class="nav-link"
-          >
-            Rol
-          </router-link>
-        </li>
-        <li v-if="$route.meta.requiresAuth == false">
-          <router-link 
-            :to="{ name: 'noticias'}" 
-            class="nav-link"
-          >
-            Anuncios
-          </router-link>
-        </li>  
-      </ul>
-    </div>
-    <div 
-      id="content" 
-      class="col-sm-12"
+    <span 
+    class="navbar-brand text-white" 
+    href="#"
     >
-      <router-view />
-    </div>
-  </div>
+    Restaurante
+  </span>
+  <ul>
+    <!-- <li v-if="usuario_logeado == ''"> -->
+      <li v-if="$route.meta.requiresAuth == false">
+        <router-link :to="{ name: 'bienvenidos'}" class="nav-link">
+          Inicio
+        </router-link>
+      </li>
+      <li v-else>
+        <router-link 
+        :to="{ name: 'role'}" 
+        class="nav-link"
+        >
+        Rol
+      </router-link>
+    </li>
+    <li v-if="$route.meta.requiresAuth == false">
+      <router-link 
+      :to="{ name: 'noticias'}" 
+      class="nav-link"
+      >
+      Anuncios
+    </router-link>
+  </li>  
+</ul>
+</div>
+<div 
+id="content" 
+class="col-sm-12"
+>
+<router-view />
+</div>
+</div>
 </template>
 <script>
-import { userMixins } from '../mixins/getuser.js'
-export default {
-  mixins: [userMixins],
-  data() {
-    return {
-      usuario_logeado: ''
-    };
-  },
-  mounted() {
-    this.getUser()
-  }
-};
+  import { userMixins } from '../mixins/getuser.js'
+  export default {
+    mixins: [userMixins],
+    data() {
+      return {
+        usuario_logeado: ''
+      };
+    },
+    mounted() {
+      this.getUser()
+    }
+  };
 </script>
 <style>
 
